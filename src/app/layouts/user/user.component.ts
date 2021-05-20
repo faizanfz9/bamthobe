f6632b3608ac3f9d10ceceaf26c3b9a742d4e237
Faizan <faizanahmad456@gmail.com>
2021-05-20 12:51:11 +0530

    changes made

--- src/app/layouts/user/user.component.ts
+++ src/app/layouts/user/user.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -10,14 +11,22 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class UserComponent implements OnInit {
   user: any;
+  showMenu = false;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private router: Router) { 
      this.user =  this.authService.getLoggedUser();
      this.authService.user.subscribe(res => {
         if(res.data) {
           this.user = res.data;
         }
      });
+
+     this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+        this.showMenu = false;
+    });
   }
 
   ngOnInit(): void {
